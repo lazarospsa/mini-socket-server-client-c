ed167831e1b50b48860d16601e3871194de1b16f
Lazaros Psarokostas <lazarospsa@hotmail.com>
2020-11-16 00:55:15 +0200

    upload the mini project

---
+++ clientsocket.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+
+#include <netinet/in.h>
+
+int main(){
+    int a;
+    a = socket(AF_INET, SOCK_STREAM, 0);
+    struct sockaddr_in mghaddress;
+    mghaddress.sin_family = AF_INET;
+    mghaddress.sin_port = htons(5000);
+    mghaddress.sin_addr.s_addr = INADDR_ANY;
+    int b;
+    b = connect(a,(struct sockaddr *)&mghaddress, sizeof(mghaddress));
+    char c[1024]; //buffer
+    recv(a, &c, sizeof(c), 0);
+    printf("%s\n", c);
+    return 0;
+}
---
+++ serversocket.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+
+#include <netinet/in.h>
+//https://www.tenouk.com/Module40c.html
+int main(){
+    int a;
+    a = socket(AF_INET, SOCK_STREAM, 0);
+    struct sockaddr_in mghaddress;
+    mghaddress.sin_family = AF_INET;
+    mghaddress.sin_port = htons(5000);
+    mghaddress.sin_addr.s_addr = INADDR_ANY;
+    int b;
+    b = bind(a, (struct sockaddr *)&mghaddress, sizeof(mghaddress));
+    listen(a, 500); //up to 5 connections
+    int conn;
+    conn = accept(a, 0, 0);
+    char d[256] = "these data are from server";
+    send(conn, d, sizeof(d), 0);
+    close(a);//close socket
+    return 0;
+}
